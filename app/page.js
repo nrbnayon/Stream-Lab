80b01efb1049e4578107d168daba4c4358c30862
mahfuzhasan2003 <mahfuzhasan2003@yahoo.com>
2025-08-14 16:17:26 +0600

    Added: Latest Drama

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import HeroCarousel from "@/components/home/hero-carousel";
+import LatestDrama from "@/components/home/latest-drama";
 import TrendingNow from "@/components/home/trending-now";
 import { moviesData } from "@/constants";
 
@@ -6,8 +7,8 @@ export default function Home() {
   return (
     <div>
       <HeroCarousel moviesData={moviesData} />
-
       <TrendingNow />
+      <LatestDrama />
 
       <div className="h-svh"></div>
     </div>
--- components/home/latest-drama.jsx
+++ components/home/latest-drama.jsx
@@ -1,10 +1,10 @@
 import { moviesData } from "@/constants";
 import MovieCard from "../movie-card";
 
-export default function TrendingNow() {
+export default function LatestDrama() {
   return (
     <div className="container mt-16">
-      <h2 className="text-4xl font-medium leading-relaxed">Trending Now</h2>
+      <h2 className="text-4xl font-medium leading-relaxed">Latest Drama</h2>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {moviesData.slice(0, 8).map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
