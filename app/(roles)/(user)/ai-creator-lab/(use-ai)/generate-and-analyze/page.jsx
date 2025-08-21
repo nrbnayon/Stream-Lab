c3eac2803536b82d1558d20b0a5ebd0630f347f2
mahfuzhasan2003 <mahfuzhasan2003@yahoo.com>
2025-08-21 17:33:02 +0600

    Fixed: useSearchParams issue.

--- app/(roles)/(user)/ai-creator-lab/(use-ai)/generate-and-analyze/page.jsx
+++ app/(roles)/(user)/ai-creator-lab/(use-ai)/generate-and-analyze/page.jsx
@@ -4,7 +4,7 @@ import AiInput from "@/components/dashboard/user/ai-creator-lab/generate-and-ana
 import TabNavigation from "@/components/dashboard/user/ai-creator-lab/generate-and-analyze/tab-navigation";
 import RecentGeneration from "@/components/dashboard/user/ai-creator-lab/recent-generation";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { Suspense, useCallback } from "react";
 
 export default function GenerateAndAnalyzePage() {
   const searchParams = useSearchParams();
@@ -24,7 +24,12 @@ export default function GenerateAndAnalyzePage() {
   return (
     <div>
       {/* Tab navigation */}
-      <TabNavigation activeTab={activeTab} handleTabChange={handleTabChange} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TabNavigation
+          activeTab={activeTab}
+          handleTabChange={handleTabChange}
+        />
+      </Suspense>
       {/* AI input */}
       <AiInput activeTab={activeTab} />
       {/* Recent Generation */}
