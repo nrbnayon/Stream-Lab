ef895dad8d6643b012545162ad4031349d8a5f93
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2025-08-28 16:39:01 +0600

    Modified: sidebar

--- components/dashboard/sign-out-btn.jsx
+++ components/dashboard/sign-out-btn.jsx
@@ -1,17 +1,58 @@
 "use client";
+
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { Logout01Icon } from "@hugeicons/core-free-icons/index";
 
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from "@/components/ui/alert-dialog";
+import { useRouter } from "next/navigation";
+
 export default function SignOutBtn() {
+  const router = useRouter();
+  const handleSignOut = () => {
+    // TODO: Sign out functionality
+    console.log("Signing out...");
+
+    router.push("/signin");
+  };
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        {/* TODO: redirect and add functionality */}
-        <SidebarMenuButton className="cursor-pointer">
-          <HugeiconsIcon icon={Logout01Icon} className="text-destructive" />
-          <span>Sign Out</span>
-        </SidebarMenuButton>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <SidebarMenuButton className="cursor-pointer" variant="destructive">
+              <HugeiconsIcon icon={Logout01Icon} />
+              <span>Sign Out</span>
+            </SidebarMenuButton>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you sure you want to sign out?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                You’ll need to log in again to access your account.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={handleSignOut}>
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </SidebarMenuItem>
     </SidebarMenu>
   );
